6d3b42d24d2b383f78739c59983e38ce32379a6a
admin <admin>
2021-03-09 22:06:17 +0800

    dcl(double chekk lock)加volatile，避免new对象时指令重排

--- src/main/java/demo/dan_li/FootBall2.java
+++ src/main/java/demo/dan_li/FootBall2.java
@@ -9,13 +9,14 @@ package demo.dan_li;
 class FootBall2 {
     private FootBall2() {}
 
-    private static FootBall2 fb;
+    // 加volatile避免指令重排
+    private static volatile FootBall2 fb;
 
     public static FootBall2 getFootBall() {
         if (fb == null) {
             synchronized (FootBall2.class) {
                 if (fb == null) {
-                    fb = new FootBall2();
+                    fb = new FootBall2();// 加volatile避免指令重排，导致可能返回半初始化的对象
                 }
             }
         }
